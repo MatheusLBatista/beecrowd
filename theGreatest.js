83005bec3e50741d6abc9bde54a1e4023dc0940c
Matheus Lucas Batista <matheusifro2020@gmail.com>
2024-09-01 20:46:49 -0400

    version 1

--- theGreatest.js
+++ theGreatest.js
@@ -2,7 +2,9 @@ let lines = ['7', '14', '106'];
 
 let a = parseInt(lines[0]);
 let b = parseInt(lines[1]);
+let c = parseInt(lines[2]);
 
-const MAIORAB = ((a + b) + abs(a-b)) / 2;
+let maiorab = ((a + b) + Math.abs(a-b)) / 2;
+let maior = (maiorab - c + Math.abs(maiorab - c)) / 2;
 
-console.log(MAIORAB);
+console.log(maior + " eh o maior");
