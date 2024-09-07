6e8c225e79b220c2169dd9fb26ecd316ceece28a
Matheus Lucas Batista <matheusifro2020@gmail.com>
2024-09-07 14:25:39 -0400

    theGreatest update

--- theGreatest.js
+++ theGreatest.js
@@ -5,6 +5,11 @@ let b = parseInt(lines[1]);
 let c = parseInt(lines[2]);
 
 let maiorab = ((a + b) + Math.abs(a-b)) / 2;
-let maior = (maiorab - c + Math.abs(maiorab - c)) / 2;
+let maiorabc = ((maiorab + c) + Math.abs(maiorab - c)) / 2
 
-console.log(maior + " eh o maior");
+if(maiorab > maiorabc) {
+    console.log(maiorab + " eh o maior")
+}
+else {
+    console.log(maiorabc + " eh o maior")
+}
